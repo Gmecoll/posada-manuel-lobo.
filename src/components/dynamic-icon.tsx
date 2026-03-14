ac10778b858dccffd6322b47e92b0bb154dadd89
Gabriel mecoll <gmecollg@gmail.com>
2026-03-14 01:05:36 +0000

    Actúa como Ingeniero Senior de Software. Mi despliegue falla por una ver

--- src/components/dynamic-icon.tsx
+++ src/components/dynamic-icon.tsx
@@ -1,24 +1,9 @@
 "use client";
-
 import React from 'react';
 import * as Icons from 'lucide-react';
-
-interface DynamicIconProps extends React.SVGProps<SVGSVGElement> {
-  name: string;
-  size?: number | string;
-  color?: string;
-}
-
-export const DynamicIcon = ({ name, ...props }: DynamicIconProps) => {
-  // @ts-ignore - Forzamos la lectura dinámica para que el build pase
-  const IconComponent = Icons[name as keyof typeof Icons];
-
-  if (!IconComponent) {
-    return null;
-  }
-
-  // @ts-ignore - Evitamos el error de "construct or call signatures"
+export const DynamicIcon = ({ name, ...props }: any) => {
+  const IconComponent = (Icons as any)[name];
+  if (!IconComponent) return null;
   return <IconComponent {...props} />;
 };
-
 export default DynamicIcon;
